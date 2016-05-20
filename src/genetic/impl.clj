ca618a38064aa1591a53a404cc13b5ab4f26e3eb
Владимир Яновский <vladimir.yanovsky@towardsmars.com>
2016-05-20 20:50:24 +0300

    Initialization

--- src/genetic/impl.clj
+++ src/genetic/impl.clj
@@ -2,14 +2,14 @@
   (:require [clojure.pprint :refer [cl-format]]))
 
 (defn random-chromosome
-  "Produces a random vector of bits"
+  "Produces a random sequence of bits"
   [n]
-  (apply vector
-         (take n (repeatedly #(rand-int 2)))))
+  (take n (repeatedly #(rand-int 2))))
 
 (defn init
-  "Returns a map with all the initial chromosomes. N is chomosome count, L is chromosome length"
-  [n l]
-  (loop [n l]))
+  "Returns a vector of maps with all the initial chromosomes and fitnesses."
+  [num-chromosomes chromosome-length default-fitness]
+  (repeatedly num-chromosomes #(hash-map :body (apply vector (random-chromosome chromosome-length))
+                                         :fitness default-fitness)))
 
 (load "impl_encoding")
